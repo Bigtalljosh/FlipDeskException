2a9134f30858dc81d525be234a3e25056426b5dd
Josh Dadak <josh.dadak@iccsolutions.com>
2016-03-31 14:11:10 +0100

    Initial Commit

---
+++ Flip.Desk/Flip.Desk/FlipException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Flip.Desk
+{
+    [Serializable]
+    public class FlipException : Exception
+    {
+        private string message = "(╯°□°）╯︵ ┻━┻   Something went wrong.... :";
+
+        public FlipException()
+        {
+        }
+
+        public FlipException(string message) : base(message)
+        {
+            this.message += base.Message;
+        }
+
+        public FlipException(string message, Exception innerException) : base(message, innerException)
+        {
+            this.message += base.Message;
+        }
+
+        protected FlipException(SerializationInfo info, StreamingContext context): base(info, context)
+        {
+            this.message += base.Message;
+        }
+    }
+}
