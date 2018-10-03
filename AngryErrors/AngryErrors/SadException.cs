22f2d5145566335a9482d72bf93b45ae8d34c799
Josh Dadak <joshdadak@gmail.com>
2018-10-03 14:44:51 +0100

    Added a sad exception :(

---
+++ AngryErrors/AngryErrors/SadException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AngryErrors
+{
+    public class SadException : Exception
+    {
+        private const string Sad = "ε(´סּ︵סּ`)з" + AngryErrorsHelpers.SomethingWrong;
+
+        public SadException()
+            : base(Sad)
+        {
+        }
+
+        public SadException(string message)
+            : base(Sad + Environment.NewLine + message)
+        {
+        }
+
+        public SadException(string message, Exception innerException)
+            : base(Sad + Environment.NewLine + message, innerException)
+        {
+        }
+    }
+}
