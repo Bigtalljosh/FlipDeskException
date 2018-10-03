b1dedbdbea8f00a6bfabb60672e262f59a97cf4e
Josh Dadak <joshdadak@gmail.com>
2018-10-03 14:51:42 +0100

    Added happy exception

---
+++ AngryErrors/AngryErrors/HappyException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AngryErrors
+{
+    public class HappyException : Exception
+    {
+        private const string Happy = "ε(´^_^')з" + AngryErrorsHelpers.SomethingWrong;
+
+        public HappyException()
+            : base(Happy)
+        {
+        }
+
+        public HappyException(string message)
+            : base(Happy + Environment.NewLine + message)
+        {
+        }
+
+        public HappyException(string message, Exception innerException)
+            : base(Happy + Environment.NewLine + message, innerException)
+        {
+        }
+    }
+}
